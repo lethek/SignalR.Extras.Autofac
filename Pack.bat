4bb124e4f4a5389d1098de6f693aa325bb870307
Michael Monsour <michael@lethek.net>
2014-07-05 14:50:19 +1000

    fixed a couple minor nuget packaging issues

--- Pack.bat
+++ Pack.bat
@@ -1,11 +1,14 @@
 @echo off
+
+if not exist "%~dp0Build" md "%~dp0Build"
+
 if "%1"=="" goto rebuildCurrent
 
-nuget pack %~dp0Source\SignalR.Extras.Autofac\SignalR.Extras.Autofac.csproj -Symbols -Build -OutputDirectory %~dp0Build -Properties Configuration=%1
+nuget pack "%~dp0Source\SignalR.Extras.Autofac\SignalR.Extras.Autofac.csproj" -Symbols -Build -OutputDirectory "%~dp0Build" -Properties Configuration=%1
 goto end
 
 :rebuildCurrent
-nuget pack %~dp0Source\SignalR.Extras.Autofac\SignalR.Extras.Autofac.csproj -Symbols -Build -OutputDirectory %~dp0Build
+nuget pack "%~dp0Source\SignalR.Extras.Autofac\SignalR.Extras.Autofac.csproj" -Symbols -Build -OutputDirectory "%~dp0Build"
 goto end
 
 :end
