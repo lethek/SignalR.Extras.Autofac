f6debac0f7ac238a43a81a73ec6935b35cf1cc2b
Michael Monsour <michael@lethek.net>
2014-07-11 11:52:54 +1000

    removed assembly culture info as I suspect it was causing intermittent assembly binding failures.

--- Source/SignalR.Extras.Autofac/Properties/AssemblyInfo.cs
+++ Source/SignalR.Extras.Autofac/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("SignalR.Extras.Autofac")]
 [assembly: AssemblyCopyright("Copyright © 2014 Michael Monsour")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.0.1")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
 [assembly: AssemblyInformationalVersionAttribute("0.2.0-beta")]
